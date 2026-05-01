dbe1d2634019f43e99af0e2420cee2399cadb178
Lucasvieirar <lucasvieiraribeiro2055@gmail.com>
2026-05-01 22:43:01 +0000

    cria pagina inicial

--- app.py
+++ app.py
@@ -1,32 +1,13 @@
 import streamlit as st 
-from pandas import read_csv
-
-st.title("Visualizador de arquivos")
 
 
-file = st.file_uploader(
-    label='Suba seu arquivo aqui!',
-    
-)
+st.title("Bem-Vindo " \
+"ao seu Editor e Conversor de arquivos")
 
-if file: 
-    if file.type == 'text/plain':
-        st.text(file.read().decode())
-    elif file.type == 'aplication/json':
-        st.json(file.read().decode())
-    elif file.type == 'image/jpeg':
-        st.image(file)
-    elif file.type == 'text/csv':
-        df = read_csv(file)
-        st.dataframe(df)
-    elif file.type == 'text/x-python':
-        st.code(file.read().decode(), language='python')
-    elif file.type == 'audio/mpeg':
-        st.audio(file)
-    elif file.type == 'video/mp4':
-        st.video(file)
-    else:
-        st.error('Formato de arquivo não suportado!')
+with st.container(border=True):
+    st.subheader("Visualizador de arquivos")
+    st.write("visualize seus arquivos de maneira limpa")
 
-else:
-    st.warning("Ainda não tenho arquivo")
+
+st.button("Edição e conversão de imagem")
+
