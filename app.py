61ae2fdd030a05348df2d7b9986b36d7e3f580a6
Lucasvieirar <lucasvieiraribeiro2055@gmail.com>
2026-05-01 23:34:01 +0000

    cria pagina inicial, cards e navegabilidade

--- app.py
+++ app.py
@@ -1,13 +1,37 @@
-import streamlit as st 
+import streamlit as st
+
+st.title("Bem-Vindo ao seu Editor e Conversor de arquivos")
 
 
-st.title("Bem-Vindo " \
-"ao seu Editor e Conversor de arquivos")
+col1, col2 = st.columns(2)
 
-with st.container(border=True):
-    st.subheader("Visualizador de arquivos")
-    st.write("visualize seus arquivos de maneira limpa")
+with col1:
+    with st.container(border=True):
+        st.subheader("Visualizador de arquivos")
+        st.write("Visualize seus arquivos de maneira limpa")
+        if st.button("Visualize", key="visualizar"):
+            st.switch_page("pages/visualizador.py")
+        
 
+with col2:
+    with st.container(border=True):
+        st.subheader("Editor e Conversão de imagens")
+        st.write("Edite suas imagens e converta para JPG e PNG")
+        if st.button("Editar", key="editar_img"):
+            st.switch_page("pages/image.py")
 
-st.button("Edição e conversão de imagem")
+col3, col4 = st.columns(2)
 
+with col3:
+    with st.container(border=True):
+        st.subheader("Editor e Conversão de vídeos")
+        st.write("Edite seus vídeos e converta para MP3 e MP4")
+        if st.button("Editar", key="editar_video"):
+            st.switch_page("pages/video.py")
+
+with col4:
+    with st.container(border=True):
+        st.subheader("Editor de PDF")
+        st.write("Edite seus PDFs")
+        if st.button("Editar", key="editar_pdf"):
+            st.switch_page("pages/pdf.py")
---
+++ pages/video.py
@@ -0,0 +1,4 @@
+import streamlit as st 
+
+
+st.title("Editor e conversor de video")
